d51fe578e9cf47ab63bbbf1e3fd45aca93949979
Jesús Sánchez <jmsr060391@gmail.com>
2024-05-12 11:24:13 -0500

    terminado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate } from 'react-router-dom'
 import useAuth from '../hooks/useAuth'
+import './styles/login.css'
 
 const Login = () => {
 
@@ -35,27 +36,28 @@ const Login = () => {
   }
 
   return (
-    <>
-      {
-        hasToken ?
-          <button onClick={handleLogout}>Logout</button>
-          :
-          <div>
-            <form onSubmit={handleSubmit(submit)}>
-              <div>
-                <label htmlFor="email">Email</label>
-                <input {...register('email')} id='email' type="email" />
-              </div>
-              <div>
-                <label htmlFor="password">Password</label>
-                <input {...register('password')} id='password' type="password" />
-              </div>
-              <button>Login</button>
-            </form>
-            <p>if you are not registered yet <Link to='/register'>Register you</Link></p>
-          </div>
-      }
-    </>
+    <div className="login-container">
+      {hasToken ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <div>
+          <h2 className="login-title">Login</h2>
+          <form onSubmit={handleSubmit(submit)}>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input {...register('email')} id='email' className='login-input' type="email" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input {...register('password')} id='password' className='login-input' type="password" />
+            </div>
+            <button className="login-button">Login</button>
+          </form>
+          <p className="login-link">If you are not registered yet <Link to='/register'>Register here</Link></p>
+        </div>
+      )}
+    </div>
   )
 }
+
 export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import useAuth from '../hooks/useAuth'
+import './styles/register.css'
 
 const Register = () => {
 
@@ -21,31 +22,63 @@ const Register = () => {
   }
 
   return (
-    <div>
+    <div className="register-container">
+      <h2 className="register-title">Register</h2>
       <form onSubmit={handleSubmit(submit)}>
         <div>
-          <label htmlFor="firstName">FirstName</label>
-          <input {...register('firstName')} id='firstName' type="text" />
+          <label htmlFor="firstName">First Name</label>
+          <input {...register('firstName')} id='firstName' className='register-input' type="text" />
         </div>
         <div>
-          <label htmlFor="lastName">LastName</label>
-          <input {...register('lastName')} id='lastName' type="text" />
+          <label htmlFor="lastName">Last Name</label>
+          <input {...register('lastName')} id='lastName' className='register-input' type="text" />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input {...register('email')} id='email' type="email" />
+          <input {...register('email')} id='email' className='register-input' type="email" />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input {...register('password')} id='password' type="password" />
+          <input {...register('password')} id='password' className='register-input' type="password" />
         </div>
         <div>
           <label htmlFor="phone">Phone</label>
-          <input {...register('phone')} id='phone' type="text" />
+          <input {...register('phone')} id='phone' className='register-input' type="text" />
         </div>
-        <button>Add User</button>
+        <button className="register-button">Register</button>
       </form>
     </div>
   )
 }
+
 export default Register
+
+//   return (
+//     <div>
+//       <form onSubmit={handleSubmit(submit)}>
+//         <div>
+//           <label htmlFor="firstName">FirstName</label>
+//           <input {...register('firstName')} id='firstName' type="text" />
+//         </div>
+//         <div>
+//           <label htmlFor="lastName">LastName</label>
+//           <input {...register('lastName')} id='lastName' type="text" />
+//         </div>
+//         <div>
+//           <label htmlFor="email">Email</label>
+//           <input {...register('email')} id='email' type="email" />
+//         </div>
+//         <div>
+//           <label htmlFor="password">Password</label>
+//           <input {...register('password')} id='password' type="password" />
+//         </div>
+//         <div>
+//           <label htmlFor="phone">Phone</label>
+//           <input {...register('phone')} id='phone' type="text" />
+//         </div>
+//         <button>Add User</button>
+//       </form>
+//     </div>
+//   )
+// }
+// export default Register
